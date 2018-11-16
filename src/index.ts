223d14921df0604cae7be2d9687181203623c299
Spencer Pollock <spencer@spollock.ca>
2018-11-16 00:53:16 -0800

    chore(tooling): Initial setup and scripts
    I have setup the initial setup files and scripts for the project (based on another one of my
    projects) and began installing and checking necessary dependencies. This is still under review and
    testing (especially with Travis-CI) and will need to go through a few iterations as well as some
    build test checking.
    
    re #2

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * A simple function to add two numbers together and return the sum.
+ * @param  {number} x first number
+ * @param  {number} y second number
+ * @returns number sum
+ */
+export function add(x: number, y: number): number {
+    return x + y;
+}
