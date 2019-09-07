9dcb48b5bf8ccefed9f47eda68623809ad8d2cff
Spencer Pollock <srepollock@users.noreply.github.com>
2019-09-06 23:13:54 -0700

    fix: Updated testing and package.json (#25)
    Now using Jest and Rollup in the project.

--- test/add.spec.ts
+++ test/add.spec.ts
@@ -1,13 +1,12 @@
-import { expect } from "chai";
 import { add } from "../src";
 
 describe("Add test", () => {
     it("should add 5 + 5 and equal 10", () => {
         let num = add(5, 5);
-        expect(num).to.equal(10);
+        expect(num).toBe(10);
     });
     it("should not equal 11 when adding 5 + 5", () => {
         let num = add(5, 5);
-        expect(num).to.not.equal(11);
+        expect(num).not.toBe(11);
     });
 });
