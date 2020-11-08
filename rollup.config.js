c6036c275d2eb47fb83afcfa0395bb27a8609c7a
Spencer Pollock <spencer@spollock.ca>
2020-11-07 21:13:44 -0800

    fix(rollup.config.js): Removed rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from "rollup-plugin-node-resolve";
 import builtins from "@joseph184/rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
 import pkg from "./package.json";
